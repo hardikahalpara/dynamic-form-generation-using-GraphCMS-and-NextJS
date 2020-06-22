967940fb1cf85df5b3ed1eacca355851c0592bd0
Hardik Ahalpara <hardikahalpara1234@gmail.com>
2020-06-22 15:36:34 +0530

    cleaned up default code

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      Head to '/contact' ot '/home' to see retrieved data respectively
+      Head to '/contact' or '/home' to see retrieved data respectively
     </div>
   )
 }
